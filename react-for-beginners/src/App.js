decdd6b05906eda154dec653a0e39ae2f419e56f
crosswalk <crosswalk0628@gmail.com>
2023-01-22 20:28:25 +0900

    Sec6.1 useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,10 +1,17 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("call an api all the time");
+  const callApi = () => {
+    console.log("call an api once");
+  };
+  useEffect(callApi, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"}></Button>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
